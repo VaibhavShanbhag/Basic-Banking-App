1a0509797a1bdd20e5e70bd6df10b50005e31f1d
Vaibhav Shanbhag <vaibhavshanbhag467@gmail.com>
2022-04-15 11:41:13 +0530

    Inserted data into table

--- app/src/main/java/com/example/moneytransferapp/UserDatabaseHelper.kt
+++ app/src/main/java/com/example/moneytransferapp/UserDatabaseHelper.kt
@@ -15,6 +15,19 @@ class UserDatabaseHelper(context: Context, factory: SQLiteDatabase.CursorFactory
         db?.execSQL(query)
 
         insertData("20220401","Vaibhav Shanbhag","vaibhavshanbhag@gmail.com",10000,db)
+        insertData("20220410","Shreyas Naik","shreyasnaik@gmail.com",7000,db)
+        insertData("20220412","Virat Sharma","viratsharma@gmail.com",15000,db)
+        insertData("20220418","Piyush Nayak","piyushnayak@gmail.com",8500,db)
+        insertData("20220420","Sourav Joshi","souravjoshi@gmail.com",20000,db)
+        insertData("20220421","Vikram Rathore","vikramrathore@gmail.com",17000,db)
+        insertData("20220423","Vishwajeet Tekale","vishwajeettekale@gmail.com",25000,db)
+        insertData("20220425","Peter Parkar","peterparkar@gmail.com",5000,db)
+        insertData("20220427","Shivam Dubey","shivamdubey@gmail.com",6500,db)
+        insertData("20220424","Sagar Iyer","sagariyer@gmail.com",3000,db)
+        insertData("20220430","Ambati Rayudu","ambatirayudu@gmail.com",20000,db)
+        insertData("20220422","Rohan Parkar","rohanparkar@gmail.com",12000,db)
+        insertData("20220433","Gaurav Singh","gauravsingh@gmail.com",2500,db)
+        insertData("20220440","Raj Singh","rajsingh@gmail.com",6000,db)
     }
 
     private fun insertData(accountNum: String, name: String, email: String, balance: Int, db: SQLiteDatabase?) {
